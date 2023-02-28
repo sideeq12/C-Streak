281131affd534f240b0447177aa862dd31e612f6
sideeq12 <abdwaheed2018@gmail.com>
2023-02-28 19:49:02 +0000

    dynamic prog

--- CppStreak/main.cpp
+++ CppStreak/main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::cout<<"Getting the user data in DM\n";
+    int numbers;
+    std::cout<<"Kindly input number of grades : ";
+    std::cin>> numbers;
+    char *pGrades = nullptr;
+    pGrades = new char[numbers];
+    for(int i = 0; i < numbers; i++){
+        std::cout <<"Enter grade #"<< i+1 << " : ";
+        std::cin >> pGrades[i];
+    }
+    for(int i = 0; i < numbers ; i++){
+        std::cout<<"The grade #"<<i+1<< " is : "<< pGrades[i]<< std::endl;
+    }
+
+    delete[] pGrades;
     return 0;
 }
